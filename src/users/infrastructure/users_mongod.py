63a2a8f15e7e7fa248a918198bad911bb33dccbe
Kiraft <77902620+Kiraft@users.noreply.github.com>
2024-05-12 02:49:48 -0600

    se cambio el endpoint de login

--- src/users/infrastructure/users_mongod.py
+++ src/users/infrastructure/users_mongod.py
@@ -1,4 +1,6 @@
 from ...mongo.connect import ConnectionMongo
+from bson import json_util, ObjectId
+import json
 
 class MongodUser:
     def __init__(self):
@@ -6,8 +8,36 @@ class MongodUser:
 
     def user_connect(self, user, pasw):
         db = self.connect.con
-        col = db["usuarios"]
 
-        doc = col.find_one({"usuario": user, "contrasena": pasw}, {'_id': False, 'empleado': False})
-        return doc
+        pipeline = [
+            {"$match": {"usuario": user, "contrasena": pasw}},
+            {"$lookup": {
+                "from": "empleados",
+                "localField": "empleado.id",
+                "foreignField": "_id",
+                "as": "empleado"
+            }},
+            {"$unwind": "$empleado"},
+            {"$lookup": {
+                "from": "tipos_empleados",
+                "localField": "empleado.tipo_empleado.id",
+                "foreignField": "_id",
+                "as": "tipo_empleado"
+            }},
+            {"$unwind": "$tipo_empleado"},
+            {"$project": {
+                "_id": 0,
+                "contrasena": 1,
+                "usuario": 1,
+                "tipo_empleado": "$tipo_empleado.tipo"
+            }}
+        ]
+
+        resultado = db["usuarios"].aggregate(pipeline)
+        json_resultado = json.loads(json_util.dumps(resultado))
+        
+        if len(json_resultado) == 0:
+            json_resultado = None
+
+        return json_resultado
 
