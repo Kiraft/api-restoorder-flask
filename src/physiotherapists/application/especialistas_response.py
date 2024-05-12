ef2e54fbe7c39e0563af6daa1213f1508030f3df
Kiraft <77902620+Kiraft@users.noreply.github.com>
2024-05-12 12:10:03 -0600

    eliminos archivos inecesarios

--- src/physiotherapists/application/especialistas_response.py
+++
@@ -1,9 +0,0 @@
-class EspecialistasResponse():
-
-    @staticmethod
-    def parsed_especialistas(raw):
-        if raw is not None:
-           
-            return raw
-        else:
-            raise Exception("Usuario no válido")
--- src/specialties/application/especialidades_response.py
+++
@@ -1,9 +0,0 @@
-class SpecialtiesResponse():
-
-    @staticmethod
-    def parsed_specialties(raw):
-        if raw is not None:
-           
-            return raw
-        else:
-            raise Exception("No hay especialidades")
